7d9c3d5cb6fb81a74d3fa13bafc3aa7e54a28f22
maithraye <maithi3096@gmail.com>
2017-06-20 16:15:16 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class hello
+{
+   public static void main (String[] args) throws java.lang.Exception
+   {
+     System.out.println("Printed for 5 times:");
+     for(int i=0;i<5;i++)
+     {
+       System.out.println("HELLO");
+     }
+  }
+}
