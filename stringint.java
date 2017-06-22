343116bd66baa260934c0a5249294ef54e5e8353
maithraye <maithi3096@gmail.com>
2017-06-22 10:38:24 +0530

    Create stringint.java

---
+++ stringint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class stringint
+{
+   public static void main (String[] args) throws java.lang.Exception
+   {
+     String n="500";
+     int i=Integer.parseInt(n);
+     System.out.println("Integer value:"+i);
+   }
+}
